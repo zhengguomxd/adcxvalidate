7436d1492f692b74872fdf37843af687a0c046dd
zhenghan <383087882@qq.com>
2017-12-06 00:28:01 +0800

    添加commonValidators

--- src/main/java/com/qunar/x/validate/XValidators.java
+++ src/main/java/com/qunar/x/validate/XValidators.java
@@ -1,11 +1,12 @@
 package com.qunar.x.validate;
 
+import com.qunar.x.validate.base.BaseValidator;
 import com.qunar.x.validate.base.XValidator;
 
 import java.util.HashMap;
 
 public class XValidators {
     public static XValidator commonValidator(){
-        return null;
+        return BaseValidator.generateValidator();
     }
 }
